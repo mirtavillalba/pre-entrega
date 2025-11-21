5f3438e88291af853473e5e843af94c78e5cde29
mirta <villalbamirta@yahoo.com.ar>
2025-11-21 20:55:50 -0300

    proyecto final

---
+++ js/productos.js
@@ -0,0 +1,29 @@
+export const productos = [
+  {
+    nombre: "Chanel",
+    img: "./img/chanel.png",
+    precio: 1000,
+    descripcion: "descripcion 1",
+  },
+  {
+    nombre: "Jazmin",
+    img: "./img/jazmin.png",
+    precio: 2000,
+    descripcion: "descripcion 2",
+  },
+  {
+    nombre: "rosa",
+    img: "./img/rosa.png",
+    precio: 3000,
+    descripcion: "descripcion 3",
+  },
+ {
+    nombre: "brand",
+    img: "./img/brand.png",
+    precio: 4000,
+    descripcion: "descripcion 3",
+  }, 
+
+
+
+];
